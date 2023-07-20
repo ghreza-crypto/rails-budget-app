7ab18cb0cbcbdf392de9fb1f711acbfda60d6d58
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-07-20 20:56:32 +0430

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "articles#index"
+  
+  devise_scope :user do
+    authenticated :user do
+      root to: 'categories#index', as: :authenticated_root
+    end
+    
+    unauthenticated do
+      root "home#index"
+    end
+  end
+  
+  
+  resources :categories do
+    resources :expenses
+  end
+    
 end
