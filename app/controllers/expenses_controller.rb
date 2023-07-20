7b49183644dfd9486c197f355cdff8f2167b2206
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-07-20 23:01:15 +0430

    Update controller for categories and expense

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,80 @@
+class ExpensesController < ApplicationController
+    before_action :set_expense, only: %i[show edit update destroy]
+    before_action :authenticate_user!
+    load_and_authorize_resource
+  
+    # GET /expenses or /expenses.json
+    def index
+      @expenses = Expense.all
+    end
+  
+    # GET /expenses/1 or /expenses/1.json
+    def show; end
+  
+    # GET /expenses/new
+    def new
+      @category = Category.find(params[:category_id])
+  
+      if @category.author_id != current_user.id
+        redirect_to categories_path, notice: 'You are not allowed to create expenses in this category'
+      end
+  
+      @expense = Expense.new
+      @title = 'New expense'
+    end
+  
+    # GET /expenses/1/edit
+    def edit; end
+  
+    # POST /expenses or /expenses.json
+    def create
+      @expense = Expense.new(author_id: current_user.id, **expense_params)
+  
+      respond_to do |format|
+        if @expense.save
+          @expense_category = ExpenseCategory.create(category_id: params[:category_id], expense_id: @expense.id)
+  
+          format.html { redirect_to category_path(params[:category_id]), notice: 'Expense was successfully created.' }
+          format.json { render :show, status: :created, location: @expense }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @expense.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # PATCH/PUT /expenses/1 or /expenses/1.json
+    def update
+      respond_to do |format|
+        if @expense.update(author_id: current_user.id, **expense_params)
+          format.html { redirect_to expense_url(@expense), notice: 'Expense was successfully updated.' }
+          format.json { render :show, status: :ok, location: @expense }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @expense.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+  
+    # DELETE /expenses/1 or /expenses/1.json
+    def destroy
+      @expense.destroy
+  
+      respond_to do |format|
+        format.html { redirect_to expenses_url, notice: 'Expense was successfully destroyed.' }
+        format.json { head :no_content }
+      end
+    end
+  
+    private
+  
+    # Use callbacks to share common setup or constraints between actions.
+    def set_expense
+      @expense = Expense.find(params[:id])
+    end
+  
+    # Only allow a list of trusted parameters through.
+    def expense_params
+      params.require(:expense).permit(:name, :amount)
+    end
+  end
